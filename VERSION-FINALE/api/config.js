4fc6ca963f49d47086d73718028c9b2239da3cf2
moetezhechmi <moetezhechmi.groun@esprit.tn>
2021-01-23 15:04:27 +0100

    first commit

--- VERSION-FINALE/api/config.js
+++
@@ -1,7 +0,0 @@
-module.exports = {
-    'SECRET': 'MySecret',
-    'DATABASE': 'mongodb://localhost:27017/LostAndFound',
-    'PORT': '3001',
-    //'host':'http://localhost:3000/'
-    'host':'http://192.168.1.18:3001/'
-};
